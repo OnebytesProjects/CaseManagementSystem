0e9745e81e2b3d72c5a86ef6a3435b03072bbbb5
Sharan <sharan.ssr123@gmail.com>
2022-01-14 02:54:13 +0530

    a

--- Web/en/assets/script/confrim.js
+++
@@ -1,9 +0,0 @@
-function cnfrmselection() {
-    let text = "Are You Sure?";
-    if (confirm(text) == true) {
-      text = "You pressed OK!";
-    } else {
-      text = "You canceled!";
-    }
-    document.getElementById("demo").innerHTML = text;
-  }
